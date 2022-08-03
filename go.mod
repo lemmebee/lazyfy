b4d16b5c570e0170174240393b2ee180156e1106
ehabshaaban <ehabsfeki@gmail.com>
2022-08-03 12:11:59 +0200

    wip

--- go.mod
+++ go.mod
@@ -3,15 +3,34 @@ module github.com/ehabshaaban/lazyfy
 go 1.18
 
 require (
-	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/lipgloss v0.5.0
 )
 
 require (
+	github.com/charmbracelet/harmonica v0.2.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.12.0 // indirect
+	github.com/rivo/uniseg v0.3.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20220722155238-128564f6959c // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
+
+require (
+	github.com/charmbracelet/bubbles v0.13.0
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/zmb3/spotify/v2 v2.2.1
-	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 )
--- lazyfy.go
+++ lazyfy.go
@@ -1,13 +1,63 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
 
-	"github.com/ehabshaaban/lazyfy/api"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/ehabshaaban/lazyfy/internal/ui"
+)
+
+var (
+	ctx = context.Background()
 )
 
 func main() {
-	log.Default().Println(api.DescribePlaylist("7IA6x0ltOvq68X4Lm7MjMN"))
-	log.Default().Println(api.PlayListFollowersCount("7IA6x0ltOvq68X4Lm7MjMN"))
-	log.Default().Println(api.PlaylistName("7IA6x0ltOvq68X4Lm7MjMN"))
+	// log.Default().Println(api.DescribePlaylist("7IA6x0ltOvq68X4Lm7MjMN"))
+	// log.Default().Println(api.PlayListFollowersCount("7IA6x0ltOvq68X4Lm7MjMN"))
+	// log.Default().Println(api.PlaylistName("7IA6x0ltOvq68X4Lm7MjMN"))
+	// playlist, msg := api.GetFeaturedPlaylists()
+	// log.Default().Println("CEREBRAL::DEBUG:::playlist", playlist)
+	// log.Default().Println("CEREBRAL::DEBUG:::msg", msg)
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", api.GetFeaturedPlaylistID())
+	// p1 := api.Playlist{
+	// 	ID: "p1_7IA6x0ltOvq68X4Lm7MjMN",
+	// }
+	// p2 := api.Playlist{
+	// 	ID: "p2_7IA6x0ltOvq68X4Lm7MjMN",
+	// }
+	// p3 := api.Playlist{
+	// 	ID: "p3_7IA6x0ltOvq68X4Lm7MjMN",
+	// }
+	// ids := []api.Playlist{p1, p2, p3}
+	// p := api.Playlist{ID: id}
+
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::DescribePlaylist", api.DescribePlaylist(p))
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::PlayListFollowersCount", api.PlayListFollowersCount(p))
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::PlaylistName", api.PlaylistName(p))
+	// _, e := api.GetFeaturedPlaylists()
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::GetFeaturedPlaylists", e)
+	// _, r := api.GetFeaturedPlaylistsWithCountry("FR")
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::GetFeaturedPlaylistsWithCountry", r)
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", api.GetPlaylistIDs(api.GetFeaturedPlaylists()))
+
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", len(api.GetPlaylistIDs(api.GetPlaylists())))
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", api.GetPlaylistIDs(api.GetPlaylists()))
+
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", len(api.GetPlaylistNames(api.GetPlaylists())))
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", api.GetPlaylistNames(api.GetPlaylists()))
+
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", len((api.GetPlaylists())))
+	// log.Default().Println("::::CEREBRAL::::DEBUG::::", api.GetPlaylists())
+
+	rand.Seed(time.Now().Unix())
+
+	if err := tea.NewProgram(ui.NewInitialModel()).Start(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
 }
