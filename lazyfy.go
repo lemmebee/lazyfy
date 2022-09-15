a17c6bc8b88f338c2da87a780b46449a87c835f4
ehabfeki <ehab.shaaban@contentsquare.com>
2022-09-15 18:21:21 +0200

    feat: Clean main

--- lazyfy.go
+++ lazyfy.go
@@ -14,33 +14,8 @@ var (
 )
 
 func main() {
-	if err := tea.NewProgram(ui.NewInitialModel(), tea.WithAltScreen()).Start(); err != nil {
+	if err := tea.NewProgram(ui.NewListModel(), tea.WithAltScreen()).Start(); err != nil {
 		fmt.Println("Error running lazyfy!:", err)
 		os.Exit(1)
 	}
-
-	// playlist := api.Playlist{
-	// 	ID: "3CddHe2KUSiOtXxFdwrUkL",
-	// }
-
-	// tracks := api.GetPlaylistTracks(playlist)
-
-	// fmt.Println(tracks[1])
-
-	// // track name
-	// trackName := tracks[1].Name
-	// fmt.Println(trackName)
-
-	// // track artist
-	// fmt.Println(tracks[1].Artists[trackName])
-
-	// ids := api.GetPlaylistIDs(api.GetPlaylists())
-	// fmt.Println(ids)
-	// playlist := api.Playlist{
-	// 	ID: spotify.ID(ids[1]),
-	// }
-	// fmt.Println(playlist)
-	// tracks := api.GetPlaylistTracks(playlist)
-	// fmt.Println(tracks)
-
 }
