18ccfb6514c81c19bb2a962152a191b4a4d395f6
EhabShaaban <ehabsfeki@gmail.com>
2022-09-23 22:24:31 +0200

    refactor(api): Add tracks

--- api/playlists.go
+++ api/playlists.go
@@ -3,16 +3,9 @@ package api
 import (
 	"context"
 	"log"
-	"strings"
 
 	"github.com/zmb3/spotify/v2"
 )
-
-type Track struct {
-	Name     string
-	Artists  map[string][]string
-	Explicit bool
-}
 
 type Playlist struct {
 	ID   string
@@ -20,8 +13,7 @@ type Playlist struct {
 }
 
 var (
-	ctx     = context.Background()
-	artists = make(map[string][]string)
+	ctx = context.Background()
 )
 
 func DescribePlaylist(playlist *Playlist) (fullPlaylist *spotify.FullPlaylist) {
@@ -55,31 +47,4 @@ func GetPlaylists() (playlists []*Playlist) {
 	}
 
 	return playlists
-}
-
-func GetPlaylistTracks(playlist *Playlist) (tracks []*Track) {
-	fullPlaylist := DescribePlaylist(playlist)
-
-	for _, track := range fullPlaylist.Tracks.Tracks {
-		trackName := track.Track.Name
-		isExplicit := track.Track.Explicit
-		simpleArtists := track.Track.Artists
-
-		for _, simpleArtist := range simpleArtists {
-			artists[trackName] = append(artists[trackName], simpleArtist.Name)
-		}
-
-		tracks = append(tracks,
-			&Track{
-				Name:     trackName,
-				Artists:  artists,
-				Explicit: isExplicit,
-			})
-	}
-
-	return tracks
-}
-
-func ConvertTrackArtistListToSingleString(artists []string) string {
-	return strings.Join(artists, ", ")
 }
---
+++ api/tracks.go
@@ -0,0 +1,40 @@
+package api
+
+import "strings"
+
+type Track struct {
+	Name     string
+	Artists  map[string][]string
+	Explicit bool
+}
+
+var (
+	artists = make(map[string][]string)
+)
+
+func GetPlaylistTracks(playlist *Playlist) (tracks []*Track) {
+	fullPlaylist := DescribePlaylist(playlist)
+
+	for _, track := range fullPlaylist.Tracks.Tracks {
+		trackName := track.Track.Name
+		isExplicit := track.Track.Explicit
+		simpleArtists := track.Track.Artists
+
+		for _, simpleArtist := range simpleArtists {
+			artists[trackName] = append(artists[trackName], simpleArtist.Name)
+		}
+
+		tracks = append(tracks,
+			&Track{
+				Name:     trackName,
+				Artists:  artists,
+				Explicit: isExplicit,
+			})
+	}
+
+	return tracks
+}
+
+func ConvertTrackArtistListToSingleString(artists []string) string {
+	return strings.Join(artists, ", ")
+}
