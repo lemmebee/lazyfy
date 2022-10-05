779983516bdb30cfcd89dc274b8cc817228b0753
EhabShaaban <ehabsfeki@gmail.com>
2022-10-05 09:23:08 +0200

    feat(ui): Add styling

--- internal/ui/common.go
+++ internal/ui/common.go
@@ -1,20 +1,26 @@
 package ui
 
 import (
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/muesli/termenv"
 )
 
 var (
-	primary    = termenv.ColorProfile().Color("205")
-	secondary  = termenv.ColorProfile().Color("#89F0CB")
-	gray       = termenv.ColorProfile().Color("#626262")
-	midGray    = termenv.ColorProfile().Color("#4A4A4A")
-	blue       = termenv.ColorProfile().Color("#F0FFFF")
-	red        = termenv.ColorProfile().Color("#FF2D00")
-	green      = termenv.ColorProfile().Color("#00FF00")
-	docStyle   = lipgloss.NewStyle().Margin(1)
-	titleStyle = lipgloss.NewStyle().MarginLeft(0)
+	primary           = termenv.ColorProfile().Color("205")
+	secondary         = termenv.ColorProfile().Color("#89F0CB")
+	gray              = termenv.ColorProfile().Color("#626262")
+	midGray           = termenv.ColorProfile().Color("#4A4A4A")
+	blue              = termenv.ColorProfile().Color("#F0FFFF")
+	red               = termenv.ColorProfile().Color("#FF2D00")
+	green             = termenv.ColorProfile().Color("#00FF00")
+	docStyle          = lipgloss.NewStyle().Margin(1)
+	titleStyle        = lipgloss.NewStyle().MarginLeft(0)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
 )
 
 const (
